7c3dd953f5c67fecd9e2957eac8d3e5b0935dbfa
denyadenya77 <jetropak7@gmail.com>
2020-01-26 13:46:37 +0200

    26/01/2020/

--- weather/urls.py
+++ weather/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
-from .views import SearchPageView, search_result_view, city_weather_view, added_city_view, user_cities_view
+from .views import (SearchPageView, DeleteCityView,
+                    CityWeatherDetailView, UserCitiesView,
+                    SearchResultView, AddedCityView)
 
 urlpatterns = [
     path('', SearchPageView.as_view(), name='search_page'),
-    path('search/', search_result_view, name='search_result_page'),
-    path('city-weather/<str:cityname>', city_weather_view, name='city_weather_view'),
-    path('added_city/<str:cityname>', added_city_view, name='added_city_view'),
-    path('user_cities_view/', user_cities_view, name='user_cities_view'),
+    path('search/', SearchResultView.as_view(), name='search_result_page'),
+    path('city_weater_class_based/<str:cityname>', CityWeatherDetailView.as_view(), name='city_weather_class_based'),
+    path('user_cities_view/', UserCitiesView.as_view(), name='user_cities_view'),
+    path('added_city/<str:cityname>', AddedCityView.as_view(), name='added_city_view'),
+    path('delete_city/<str:cityname>', DeleteCityView.as_view(), name='delete_city_view'),
 ]
--- weather/views.py
+++ weather/views.py
@@ -1,9 +1,7 @@
-from django.shortcuts import render
-from django.views.generic import TemplateView
-import requests
-from datetime import datetime
-from django.contrib.auth.decorators import login_required
 from .models import UserCities
+from django.views.generic import TemplateView
+from .get_weather import Weather, APIError
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
 
@@ -12,83 +10,99 @@ class SearchPageView(TemplateView):
     template_name = 'home.html'
 
 
-def search_result_view(request):
-    city_name = request.GET.get('q')
-    return render(request, 'search_results.html', {'city_name': city_name})
+class SearchResultView(TemplateView):
 
+    template_name = 'search_results.html'
 
-def city_weather_view(request, cityname):
-    city = cityname
-    url = f'http://api.openweathermap.org/data/2.5/forecast?q={city}&appid=4b0474b68bb0012daed655d182e69e2a&units=metric'
-    res = requests.get(url)
-    weather = res.json()
-
-    # get today's date
-    now = datetime.now()
-    dt_string = now.strftime("%d")
-    date_to_view = str(now)[:10]
-
-    five_days_list = weather['list']
-
-    # air temperature for today
-    current_weather = weather['list'][0]['main']['temp']
-
-    weather_ready = []
-
-    for day in five_days_list:
-
-        date = day['dt_txt'][8:10]
-        time = day['dt_txt'][11:13]
-
-        if time == '12' and date != dt_string:
-            day['dt_txt'] = day['dt_txt'][:10]
-            weather_ready.append(day)
-
-    context = {
-        'city': city,
-        'today_day': date_to_view,
-        'today_temp': current_weather,
-        'weather_ready': weather_ready,
-    }
-
-    return render(request, 'city_weather.html', context=context)
-
-@login_required
-def added_city_view(request, cityname):
-    city = cityname
-    current_user = request.user
-
-    city_to_db = UserCities(user=current_user, city=city)
-    city_to_db.save()
-
-    context = {
-        'city': city
-    }
-    return render(request, 'city_added.html', context=context)
-
-
-@login_required
-def user_cities_view(request):
-    current_user = request.user
-
-    user_cities_list = UserCities.objects.filter(user=current_user)
-
-    context = {
-        'city_list': []
-    }
-
-    for city in user_cities_list:
-
-        url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid=4b0474b68bb0012daed655d182e69e2a&units=metric'
-        res = requests.get(url)
-        weather = res.json()
-
-        city_dict = {
-            'name': weather['name'],
-            'temp': weather['main']['temp']
+    def get_context_data(self, *args, **kwargs):
+        city = self.request.GET.get('q')
+        context = {
+            'city': city
         }
+        return context
 
-        context['city_list'].append(city_dict)
 
-    return render(request, 'user_cities_view.html', context=context)
+class CityWeatherDetailView(TemplateView):
 
+    template_name = 'city_weater_class_based.html'
+
+    def user_check(self):
+        if self.request.user.is_authenticated:
+            current_user = self.request.user
+        else:
+            current_user = None
+        return current_user
+
+    def city_in_favourites(self, current_user, cityname):
+        if current_user:
+            cities = current_user.usercities_set.all()
+            city_in_favourites = cities.filter(city__iexact=cityname).exists()
+            return city_in_favourites
+        else:
+            return True
+
+    def get_context_data(self, *args, **kwargs):
+        city = kwargs['cityname']
+
+        weather = Weather()
+
+        try:
+            context = weather.get_five_days_weather(city)
+        except APIError:
+            context = {
+                'error': 'APIError'
+            }
+        else:
+            current_user = self.user_check()
+            context['city_in_favourites'] = self.city_in_favourites(current_user, city)
+
+        return context
+
+
+class AddedCityView(LoginRequiredMixin, TemplateView):
+    template_name = 'city_added.html'
+
+    def get_context_data(self, *args, **kwargs):
+        city = kwargs['cityname']
+        current_user = self.request.user
+
+        city_to_db = UserCities(user=current_user, city=city)
+        city_to_db.save()
+
+        context = {
+            'city': city
+        }
+        return context
+
+
+class UserCitiesView(LoginRequiredMixin, TemplateView):
+
+    template_name = 'user_cities_view.html'
+
+    def user_check(self):
+        current_user = self.request.user
+        return current_user
+
+    def get_context_data(self, *args, **kwargs):
+        weather = Weather()
+        current_user = self.user_check()
+
+        try:
+            context = weather.get_current_weather(current_user)
+        except APIError:
+            context = {
+                'error': 'APIError'
+            }
+        return context
+
+
+class DeleteCityView(LoginRequiredMixin, TemplateView):
+    template_name = 'city_deleted.html'
+
+    def get_context_data(self, *args, **kwargs):
+        city = kwargs['cityname']
+        current_user = self.request.user
+
+        city_to_delete = UserCities.objects.get(user=current_user, city__iexact=city)
+        if city in city_to_delete.city:
+            city_to_delete.delete()

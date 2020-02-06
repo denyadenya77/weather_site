e50eeeb92be3ccdce2105ee5206f33c3a99fd9e0
denyadenya77 <jetropak7@gmail.com>
2020-02-06 18:18:09 +0200

    second commit

--- weather/get_weather.py
+++ weather/get_weather.py
@@ -1,5 +1,4 @@
 import requests
-import datetime
 from datetime import datetime
 
 
@@ -9,8 +8,8 @@ class APIError(Exception):
 
 class Weather:
 
-    base_url = 'http://api.openweathermap.org'
-    app_id = '4b0474b68bb0012daed655d182e69e2a'
+    base_url = "http://api.openweathermap.org"
+    app_id = "4b0474b68bb0012daed655d182e69e2a"
 
     def send_request(self, url):
         res = requests.get(url)
@@ -22,30 +21,28 @@ class Weather:
 
     def get_current_weather(self, user_cities_list):
 
-        url_type = 'current_weather'
+        url_type = "current_weather"
 
-        context = {
-            'city_list': []
-        }
+        context = {"city_list": []}
 
         for city in user_cities_list:
             url = self.create_url(city, url_type)
             weather = self.send_request(url)
 
             city_dict = {
-                'name': city.city,
-                'temp': weather['main']['temp'],
-                'wind': weather['wind']['speed'],
-                'description': weather['weather'][0]['description'],
+                "name": city.city,
+                "temp": weather["main"]["temp"],
+                "wind": weather["wind"]["speed"],
+                "description": weather["weather"][0]["description"],
             }
 
-            context['city_list'].append(city_dict)
+            context["city_list"].append(city_dict)
 
         return context
 
     def get_five_days_weather(self, city):
 
-        url_type = 'five_days_weather'
+        url_type = "five_days_weather"
 
         url = self.create_url(city, url_type)
 
@@ -56,41 +53,39 @@ class Weather:
         dt_string = now.strftime("%d")
         date_to_view = str(now)[:10]
 
-        five_days_list = weather['list']
+        five_days_list = weather["list"]
 
         # air temperature for today
-        current_weather = weather['list'][0]['main']['temp']
-        current_wind = weather['list'][0]['wind']['speed']
-        current_description = weather['list'][0]['weather'][0]['description']
+        current_weather = weather["list"][0]["main"]["temp"]
+        current_wind = weather["list"][0]["wind"]["speed"]
+        current_description = weather["list"][0]["weather"][0]["description"]
 
         weather_ready = []
 
         for day in five_days_list[1:]:
 
-            date = day['dt_txt'][8:10]
-            time = day['dt_txt'][11:13]
+            date = day["dt_txt"][8:10]
+            time = day["dt_txt"][11:13]
 
-            if time == '12' and date != dt_string:
-                day['dt_txt'] = day['dt_txt'][:10]
+            if time == "12" and date != dt_string:
+                day["dt_txt"] = day["dt_txt"][:10]
                 weather_ready.append(day)
 
         context = {
-            'city': city,
-            'today_day': date_to_view,
-            'today_temp': current_weather,
-            'current_wind': current_wind,
-            'current_description': current_description,
-            'weather_ready': weather_ready,
+            "city": city,
+            "today_day": date_to_view,
+            "today_temp": current_weather,
+            "current_wind": current_wind,
+            "current_description": current_description,
+            "weather_ready": weather_ready,
         }
 
         return context
 
     def create_url(self, city, url_type):
-        if url_type is 'current_weather':
-            url = f'{self.base_url}/data/2.5/weather?q={city}&appid={self.app_id}&units=metric'
+        if url_type is "current_weather":
+            url = f"{self.base_url}/data/2.5/weather?q={city}&appid={self.app_id}&units=metric"
             return url
-        elif url_type is 'five_days_weather':
-            url = f'{self.base_url}/data/2.5/forecast?q={city}&appid={self.app_id}&units=metric'
+        elif url_type is "five_days_weather":
+            url = f"{self.base_url}/data/2.5/forecast?q={city}&appid={self.app_id}&units=metric"
             return url
-
-
--- weather/views.py
+++ weather/views.py
@@ -7,24 +7,22 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 class SearchPageView(TemplateView):
-    template_name = 'home.html'
+    template_name = "home.html"
 
 
 class SearchResultView(TemplateView):
 
-    template_name = 'search_results.html'
+    template_name = "search_results.html"
 
     def get_context_data(self, *args, **kwargs):
-        city = self.request.GET.get('q')
-        context = {
-            'city': city
-        }
+        city = self.request.GET.get("q")
+        context = {"city": city}
         return context
 
 
 class CityWeatherDetailView(TemplateView):
 
-    template_name = 'city_weater_class_based.html'
+    template_name = "city_weater_class_based.html"
 
     def city_in_favourites(self, current_user, cityname):
         if current_user.is_authenticated:
@@ -35,42 +33,38 @@ class CityWeatherDetailView(TemplateView):
             return True
 
     def get_context_data(self, *args, **kwargs):
-        city = kwargs['cityname']
+        city = kwargs["cityname"]
 
         weather = Weather()
 
         try:
             context = weather.get_five_days_weather(city)
         except APIError:
-            context = {
-                'error': 'APIError'
-            }
+            context = {"error": "APIError"}
         else:
             current_user = self.request.user
-            context['city_in_favourites'] = self.city_in_favourites(current_user, city)
+            context["city_in_favourites"] = self.city_in_favourites(current_user, city)
 
         return context
 
 
 class AddedCityView(LoginRequiredMixin, TemplateView):
-    template_name = 'city_added.html'
+    template_name = "city_added.html"
 
     def get_context_data(self, *args, **kwargs):
-        city = kwargs['cityname']
+        city = kwargs["cityname"]
         current_user = self.request.user
 
         city_to_db = UserCities(user=current_user, city=city)
         city_to_db.save()
 
-        context = {
-            'city': city
-        }
+        context = {"city": city}
         return context
 
 
 class UserCitiesView(LoginRequiredMixin, TemplateView):
 
-    template_name = 'user_cities_view.html'
+    template_name = "user_cities_view.html"
 
     def get_context_data(self, *args, **kwargs):
         weather = Weather()
@@ -80,17 +74,15 @@ class UserCitiesView(LoginRequiredMixin, TemplateView):
         try:
             context = weather.get_current_weather(user_cities_list)
         except APIError:
-            context = {
-                'error': 'APIError'
-            }
+            context = {"error": "APIError"}
         return context
 
 
 class DeleteCityView(LoginRequiredMixin, TemplateView):
-    template_name = 'city_deleted.html'
+    template_name = "city_deleted.html"
 
     def get_context_data(self, *args, **kwargs):
-        city = kwargs['cityname']
+        city = kwargs["cityname"]
         current_user = self.request.user
 
         city_to_delete = UserCities.objects.get(user=current_user, city__iexact=city)
